e9cc5a6efb6d19ee192b04486df0bf6cb447cf98
Moskalev Nikita <mozgg2002@gmail.com>
2023-03-11 10:19:48 +0300

    add mac os specific code

--- source/railway/core/core.hpp
+++ source/railway/core/core.hpp
@@ -2,8 +2,12 @@
 
 #include <cstdint>
 
+namespace railway {
+
 using ushort = std::uint16_t;  // NOLINT
 
 using uint = std::uint32_t;  // NOLINT
 
 using ulong = std::uint64_t;  // NOLINT
+
+}  // namespace railway
--- source/railway/platform/platform.cpp
+++ source/railway/platform/platform.cpp
@@ -147,11 +147,135 @@ void DequeueHandle(int poll, int handle) {
 
 }  // namespace railway
 
-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-
-#error Platform not supported
-
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+
+#include <arpa/inet.h>
+
+#include <sys/event.h>
+#include <sys/socket.h>
+
+#include <alloca.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+namespace railway {
+
+namespace platform {
+
+int Connect(uint address, ushort port) {
+  int handle{};
+  int result{};
+
+  struct sockaddr_in peer = {
+      .sin_family = AF_INET,
+      .sin_port = htons(port),
+      .sin_addr = {
+          .s_addr = htonl(address)
+      }
+  };
+
+  handle = socket(AF_INET, SOCK_STREAM, 0);
+  if (handle == -1) {
+    return -1;  // TODO: Return error "unable to make socket"
+  }
+
+  result = connect(handle, (const struct sockaddr*)&peer, sizeof(struct sockaddr_in));
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to establish connection"
+  }
+
+  result = fcntl(handle, F_GETFL);
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to mark socket non-blocking"
+  }
+
+  result = fcntl(handle, F_SETFL, result | O_NONBLOCK);
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to mark socket non-blocking"
+  }
+
+  return handle;
+}
+
+int Listen(uint address, ushort port) {
+  int handle{};
+  int result{};
+
+  struct sockaddr_in peer = {
+      .sin_family = AF_INET,
+      .sin_port = htons(port),
+      .sin_addr = {
+          .s_addr = htonl(address)
+      }
+  };
+
+  handle = socket(AF_INET, SOCK_STREAM, 0);
+  if (handle == -1) {
+    return -1;  // TODO: Return error "unable to make socket"
+  }
+
+  result = bind(handle, (const struct sockaddr*)&peer, sizeof(struct sockaddr_in));
+  if (result == -1) {
+    close(handle);
+    return -1;  // TODO: Return error "unable to bind endpoint to socket"
+  }
+
+  result = listen(handle, SOMAXCONN);
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to mark socket as listener"
+  }
+
+  result = fcntl(handle, F_GETFL);
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to mark socket non-blocking"
+  }
+
+  result = fcntl(handle, F_SETFL, result | O_NONBLOCK);
+  if (result == -1) {
+    close(handle);
+    return -1; // TODO: Return error "unable to mark socket non-blocking"
+  }
+
+  return handle;
+}
+
+void Close(int handle) {
+  close(handle);
+}
+
+ulong Read(int handle, char* buffer, ulong size) {
+  return read(handle, buffer, size);
+}
+
+ulong Write(int handle, const char* buffer, ulong size) {
+  return write(handle, buffer, size);
+}
+
+int MakePoll() {
+}
+
+ulong Poll(int poll, Event* event, ulong size) {
+
+}
+
+void EnqueueHandle(int poll, int handle, void* state) {
+
+}
+
+void DequeueHandle(int poll, int handle) {
+}
+
+}  // namespace platform
+
+}  // namespace railway
+
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 
 #error Platform not supported
 
